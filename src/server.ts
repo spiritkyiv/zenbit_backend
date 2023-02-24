39e83bfcc7c3d6ef0d6abaf3693f2ee59031a12f
SpiritKyiv <baolt.king@gmail.com>
2023-02-24 15:09:06 +0200

    add netlify lambda

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,37 @@
+// import express from 'express';
+// import { json } from 'body-parser';
+// import cors from 'cors';
+// import userRouter from './routes/UsersMessgae';
+
+// const app = express();
+// const PORT = process.env.PORT || 8080;
+
+// app.use(json());
+// app.use(cors());
+
+// app.use('/users', userRouter);
+
+// app.get('/', (req, res) => {
+//   res.status(200).json("I'm work");
+// });
+
+// app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+
+
 import express from 'express';
-import { json } from 'body-parser';
 import cors from 'cors';
+import serverless from 'serverless-http';
 import userRouter from './routes/UsersMessgae';
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const basicRout = (enpoint: string) => `/.netlify/functions/server${enpoint}`;
 
-app.use(json());
 app.use(cors());
 
-app.use('/users', userRouter);
-
-app.get('/', (req, res) => {
+app.get(basicRout('/'), (req, res) => {
   res.status(200).json("I'm work");
 });
 
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+app.use(basicRout('/users'), userRouter);
+
+export const handler = serverless(app);
