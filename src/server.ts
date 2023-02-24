087b53bb847fd0d4dc66cdb8140cf975c736ce2e
SpiritKyiv <baolt.king@gmail.com>
2023-02-24 15:19:44 +0200

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,3 @@
-// import express from 'express';
-// import { json } from 'body-parser';
-// import cors from 'cors';
-// import userRouter from './routes/UsersMessgae';
-
-// const app = express();
-// const PORT = process.env.PORT || 8080;
-
-// app.use(json());
-// app.use(cors());
-
-// app.use('/users', userRouter);
-
-// app.get('/', (req, res) => {
-//   res.status(200).json("I'm work");
-// });
-
-// app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
-
-
 import express from 'express';
 import cors from 'cors';
 import serverless from 'serverless-http';
