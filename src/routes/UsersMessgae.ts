ad78654ffed3a18e16de70708ab2fe1fe09054e5
SpiritKyiv <baolt.king@gmail.com>
2023-02-24 19:00:23 +0200

    fix

--- src/routes/UsersMessgae.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { UserMessageController } from '../controller/UserMessgeController';
-
-const router = express.Router();
-
-router.post('/', UserMessageController.create);
-router.get('/', UserMessageController.getAll);
-
-export default router;
